b272cc007ce7c7c85b8279e610733bb9bc4f24e9
Ricardo Cardozo <112267530+Ricardo-Cardozo@users.noreply.github.com>
2023-01-25 00:28:30 -0300

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const ThoughtRoute = require("./routes/ThoughtRoute");
 const app = express();
 
 //middlewares
-app.use(cors({ credentials: true, origin: "https://front-thoughts.onrender.com/" }));
+app.use(cors({ credentials: true, origin: "https://front-thoughts.onrender.com" }));
 
 app.use(
   express.urlencoded({
